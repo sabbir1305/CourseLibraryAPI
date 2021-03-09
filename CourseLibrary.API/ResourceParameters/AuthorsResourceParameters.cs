902e1732f3c33e917c52be2ae18360c26e208527
Sabbir Ahmed Shubo <sabbir.ove@gmail.com>
2021-03-09 23:42:58 +0600

    Paging: PagedClass of T added

--- CourseLibrary.API/ResourceParameters/AuthorsResourceParameters.cs
+++ CourseLibrary.API/ResourceParameters/AuthorsResourceParameters.cs
@@ -7,7 +7,15 @@ namespace CourseLibrary.API.ResourceParameters
 {
     public class AuthorsResourceParameters
     {
+        const int MaxSize = 20;
         public string MainCategory { get; set; }
         public string SearchQuery { get; set; }
+
+        public int PageNumber { get; set; } = 1;
+        public int PageSize {
+            get => _pageSize;
+            set =>_pageSize= (value > MaxSize) ? MaxSize : value;
+        }
+        private int _pageSize=10;
     }
 }
--- CourseLibrary.API/Services/CourseLibraryRepository.cs
+++ CourseLibrary.API/Services/CourseLibraryRepository.cs
@@ -1,5 +1,6 @@
 ﻿using CourseLibrary.API.DbContexts;
 using CourseLibrary.API.Entities;
+using CourseLibrary.API.Helpers;
 using CourseLibrary.API.ResourceParameters;
 using System;
 using System.Collections.Generic;
@@ -123,18 +124,13 @@ namespace CourseLibrary.API.Services
             return _context.Authors.ToList<Author>();
         }
 
-        public IEnumerable<Author> GetAuthors(AuthorsResourceParameters authorsResourceParameters)
+        public PagedList<Author> GetAuthors(AuthorsResourceParameters authorsResourceParameters)
         {
             if (authorsResourceParameters == null)
             {
                 throw new ArgumentNullException(nameof(authorsResourceParameters));
             }
 
-            if (string.IsNullOrWhiteSpace(authorsResourceParameters.MainCategory)
-                 && string.IsNullOrWhiteSpace(authorsResourceParameters.SearchQuery))
-            {
-                return GetAuthors();
-            }
 
             var collection = _context.Authors as IQueryable<Author>;
 
@@ -153,7 +149,9 @@ namespace CourseLibrary.API.Services
                     || a.LastName.Contains(searchQuery));
             }
 
-            return collection.ToList();
+            return PagedList<Author>.Create(
+                collection, authorsResourceParameters.PageNumber,
+                authorsResourceParameters.PageSize);
         }
 
         public IEnumerable<Author> GetAuthors(IEnumerable<Guid> authorIds)
